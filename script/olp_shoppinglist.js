a58d65e4d1e203ef791fc75295c1131798dfb3aa
andela-fadebayo <fiyinfoluwa.adebayo@andela.co>
2014-11-26 17:21:20 +0100

    shopping functions working properly

--- script/olp_shoppinglist.js
+++ script/olp_shoppinglist.js
@@ -20,20 +20,24 @@ var ShoppingList = {
   //define all functions
 
   //function to create new item
-  createNewItem: function (item) {  //add item1, item2 and item3 to cater for quantity and cost in the values passed into the function here.
+  createNewItem: function (item, quantity, cost) {  //add item1, item2 and item3 to cater for quantity and cost in the values passed into the function here.
                    //create a new list item
                    var listItem = document.createElement("li");
                      var checkBox = document.createElement("input");
-                     var label = document.createElement("label");
-                     //define variables here for quantity and cost
+                     var labelItem = document.createElement("label");
+                     var labelQuantity = document.createElement("label");
+                     var labelCost = document.createElement("label");                     
                      var editItemInput = document.createElement("input");
+                     var editQuantityInput = document.createElement("input");
+                     var editCostInput = document.createElement("input");
                      var editButton = document.createElement("button");
                      var deleteButton = document.createElement("button");
 
                     //set modification parameters for each item
-
                     checkBox.type = "checkbox";
                     editItemInput.type = "text";
+                    editQuantityInput.type = "text";
+                    editCostInput.type = "text";
 
                     editButton.innerText = "Edit";
                     editButton.className = "edit";
@@ -41,18 +45,21 @@ var ShoppingList = {
                     deleteButton.innerText = "Delete";
                     deleteButton.className = "delete";
 
-                    label.innerText = item;
-                    //set label.innerText for quantity and cost here
+                    //place the item, its quantity and cost into each label
+                    labelItem.innerText = item;
+                    labelQuantity.innerText = quantity;
+                    labelCost.innerText = cost;
 
                     //append each item 
                     listItem.appendChild(checkBox);
-                    listItem.appendChild(label);
+                    listItem.appendChild(labelItem);
+                    listItem.appendChild(labelQuantity);
+                    listItem.appendChild(labelCost);
                     listItem.appendChild(editItemInput);
+                    listItem.appendChild(editQuantityInput);
+                    listItem.appendChild(editCostInput);
                     listItem.appendChild(editButton);
-                    listItem.appendChild(deleteButton);
-                    // append child for quantity and cost. ensure to follow the right order in which they should appear
-                    // listItem.appendChild(checkBox);
-                    // listItem.appendChild(checkBox);
+                    listItem.appendChild(deleteButton);                  
 
                     return listItem;
                  },
@@ -61,45 +68,69 @@ var ShoppingList = {
              console.log("Add item...");
 
              //add new list item             
-             var newListItem = ShoppingList.createNewItem(ShoppingList.newItem.value);
-
+             var newListItem = ShoppingList.createNewItem(ShoppingList.newItem.value, ShoppingList.itemQuantity.value, ShoppingList.itemCost.value);
+        
              //append this new list item to itemsListHolder
              ShoppingList.itemsListHolder.appendChild(newListItem);
              ShoppingList.bindItemEvents(newListItem, ShoppingList.itemsChecked);
 
              ShoppingList.newItem.value = "";
+             ShoppingList.itemQuantity.value = "";
+             ShoppingList.itemCost.value = "";
            },
   editItem: function () {
               console.log("Edit item...");
 
               var listItem = this.parentNode;
-              var editInput = listItem.querySelector("input[type=text]");
-              var label = listItem.querySelector("label");
+
+              var editItemInput = listItem.querySelector(".itemA");
+              var editQuantityInput = listItem.querySelector("itemB");
+              var editCostInput = listItem.querySelector("itemC");
+              var labelItem = listItem.querySelector("label");
+              var labelQuantity = listItem.querySelector("label");
+              var labelCost = listItem.querySelector("label");
               //include labels for quantity and cost here
+
+              var containsClass = listItem.classList.contains("editMode");
+       
+              //check if class of the parent is editMode
+              if (containsClass) {
+                labelItem.innerText = editItemInput.value;
+                labelQuantity.innerText = editQuantityInput.value;
+                labelCost.innerText = editCostInput.value;
+              }
+              else {
+                editItemInput.value = labelItem.innerText;
+                editQuantityInput.value = labelQuantity.innerText;
+                editCostInput.value = labelCost.innerText;
+              }
+
+              //Toggle list item from edit mode to saved mode
+              listItem.classList.toggle("editMode");
            },
   deleteItem: function () {
                 console.log("Delete item...");
 
                 var listItem = this.parentNode;
-
+                var ol = listItem.parentNode;
+                ol.removeChild(listItem);
               },
   itemsChecked: function () {
                   console.log("item checked...");
+
+                  var listItem = this.parentNode;
+                  ShoppingList.checkedOffHolder.appendChild(listItem);
+                  ShoppingList.bindItemEvents(listItem, ShoppingList.itemsUnchecked);
                 },
   itemsUnchecked: function () {
                     console.log("item unchecked...");
+
+                    var listItem = this.parentNode;
+                    ShoppingList.itemsListHolder.appendChild(listItem);
+                    ShoppingList.bindItemEvents(listItem, ShoppingList.itemsChecked);
                   },
   //function to bind item events 
   bindItemEvents: function (listItems, checkBoxEventHandler) {  
-                        // console.log("bind list items events test");
-
-                        // console.log(listItems);
-                        //select list items events tasks (listItems') children
-                        //bind editItem to editButton
-                        //bind deleteItem to deleteButton
-                        //bind checkBoxEvenHandler to checkBox
-
-
                         var checkBox = listItems.querySelector("input[type=checkbox]");
                         var editButton = listItems.querySelector("button.edit");
                         var deleteButton = listItems.querySelector("button.delete");
